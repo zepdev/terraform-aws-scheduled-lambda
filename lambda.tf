3f9a0418c415a886ddac66902e785f020a78876a
Fabian Schuetz <fabian.schuetz.extern@z-lab.com>
2019-08-08 09:28:41 +0200

    add cloudwatch logs

--- lambda.tf
+++ lambda.tf
@@ -44,5 +44,38 @@ resource "aws_lambda_function" "function" {
   environment {
     variables = var.lambda_variables
   }
-  depends_on = [data.archive_file.package]
+  depends_on = [data.archive_file.package, aws_cloudwatch_log_group.lambda_logging]
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logging" {
+  name              = "/aws/lambda/${var.name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.name}-lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = "${aws_iam_role.lambdaRole.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
 }
